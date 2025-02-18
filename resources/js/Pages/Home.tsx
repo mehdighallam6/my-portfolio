725a43a52559922c8258acc5b5d516e66dde89da
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-18 06:18:35 +0100

    Update home

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,12 +1,12 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import {TextReveal} from "@/Components/magicui/text-reveal";
 
 export default function Home() {
     return (
         <>
             <Head title="Home" />
-            <div>
-                hello
+            <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
+                <TextReveal text="My portfolio website is launching soon"/>
             </div>
         </>
     );
