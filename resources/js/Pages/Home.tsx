162ec8936ecfa183647b15f67083d232b8ab3678
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-18 06:29:14 +0100

    Add dark mode

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,12 +1,19 @@
 import { Head } from '@inertiajs/react';
 import {TextReveal} from "@/Components/magicui/text-reveal";
+import {ModeToggle} from "@/Components/mode-toggle";
 
 export default function Home() {
     return (
         <>
             <Head title="Home" />
-            <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
-                <TextReveal text="My portfolio website is launching soon"/>
+            <div className="flex flex-col items-center">
+                <div className="my-5">
+                    <ModeToggle/>
+                </div>
+                <div
+                    className="z-10 flex min-h-64 items-center justify-center bg-white dark:bg-black">
+                    <TextReveal text="My portfolio website is launching soon"/>
+                </div>
             </div>
         </>
     );
