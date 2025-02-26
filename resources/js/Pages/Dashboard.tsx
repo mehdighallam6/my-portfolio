a4c5760606a583c65afc399bfdb1c1dab3edb28c
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-26 21:07:57 +0000

    Clean

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -5,15 +5,8 @@ export default function Dashboard() {
     return (
         <SideBarLayout>
             <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
+            <div>
+                Dashboard
             </div>
         </SideBarLayout>
     );
