d9905df2b8bfce6fda928168f6b78de4d9071cab
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-26 21:05:01 +0000

    Update Dashboard.tsx

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,15 +1,9 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import SideBarLayout from '@/Layouts/SideBarLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
-                </h2>
-            }
-        >
+        <SideBarLayout>
             <Head title="Dashboard" />
 
             <div className="py-12">
@@ -21,6 +15,6 @@ export default function Dashboard() {
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </SideBarLayout>
     );
 }
