ccd14818db90400afc908c5076ef53764592814b
mehdighallam6 <mehdighallam6@gmail.com>
2025-02-28 20:05:07 +0000

    Login button in home

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,7 +1,9 @@
-import { Head } from '@inertiajs/react';
+import {Head, router} from '@inertiajs/react';
 import {TextReveal} from "@/Components/magicui/text-reveal";
 import {ModeToggle} from "@/Components/mode-toggle";
 import {ShinyButton} from "@/Components/magicui/shiny-button";
+import {Button} from "@/Components/ui/button";
+import {ChevronLeftIcon, ChevronRightIcon} from "lucide-react";
 
 export default function Home() {
 
@@ -12,20 +14,33 @@ export default function Home() {
         }
     };
 
+
+    const goToLogin = () => {
+        router.get(route('login'));
+    };
+
     return (
         <>
             <Head title="Home" />
-            <div className="flex flex-col items-center bg-white dark:bg-black">
-                <div className="my-5 flex flex-col items-center gap-3">
-                    <ModeToggle/>
-                    <ShinyButton onClick={scrollToBottom}>Scroll</ShinyButton>
+            <div className="bg-white dark:bg-black">
+                <div className="flex flex-col items-center mx-auto max-w-7xl">
+                    <div className="w-full flex flex-row-reverse my-5" onClick={goToLogin}>
+                        <Button variant="ghost">
+                            Sign in
+                            <ChevronRightIcon size={16} aria-hidden="true" />
+                        </Button>
+                    </div>
+                    <div className="my-5 flex flex-col items-center gap-3">
+                        <ModeToggle/>
+                        <ShinyButton onClick={scrollToBottom}>Scroll</ShinyButton>
+                    </div>
+                    <div
+                        className="z-10 flex min-h-64 items-center justify-center">
+                        <TextReveal text="My portfolio website is launching soon"/>
+                    </div>
                 </div>
-                <div
-                    className="z-10 flex min-h-64 items-center justify-center">
-                    <TextReveal text="My portfolio website is launching soon"/>
-                </div>
+                <div className="bg-white dark:bg-black footer"></div>
             </div>
-            <div className="bg-white dark:bg-black footer"></div>
         </>
     );
 }
