3fd0f43faa7988d6375c2eedd5876d318d2d5a35
mehdighallam6 <mehdighallam6@gmail.com>
2025-03-04 22:16:28 +0000

    Fix typo

--- resources/js/Pages/Users/Index.tsx
+++ resources/js/Pages/Users/Index.tsx
@@ -1,7 +1,7 @@
 import SideBarLayout from '@/Layouts/SideBarLayout';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard() {
+export default function Index() {
     return (
         <SideBarLayout>
             <Head title="Users" />
