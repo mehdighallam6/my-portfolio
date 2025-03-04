47b877c350f73ccaced5ec1b01be25b0f18fb35e
mehdighallam6 <mehdighallam6@gmail.com>
2025-03-04 22:06:30 +0000

    Dashboard header

--- resources/js/Components/app-sidebar.tsx
+++ resources/js/Components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import {Home, Inbox, Users} from "lucide-react"
+import {GalleryVerticalEnd, Home, Inbox, Users} from "lucide-react"
 
 import {
     Sidebar,
@@ -6,6 +6,7 @@ import {
     SidebarGroup,
     SidebarGroupContent,
     SidebarGroupLabel,
+    SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
@@ -34,6 +35,22 @@ const items = [
 export function AppSidebar() {
     return (
         <Sidebar>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <a href="#">
+                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                                    <GalleryVerticalEnd className="size-4" />
+                                </div>
+                                <div className="flex flex-col gap-0.5 leading-none">
+                                    <span className="font-semibold">My Portfolio</span>
+                                </div>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
                     <SidebarGroupContent>
--- resources/js/Layouts/SideBarLayout.tsx
+++ resources/js/Layouts/SideBarLayout.tsx
@@ -1,14 +1,31 @@
-import { SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar"
+import {SidebarInset, SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar"
 import { AppSidebar } from "@/Components/app-sidebar"
+import {ModeToggle} from "@/Components/mode-toggle";
+import {Separator} from "@/Components/ui/separator";
+
+function Toaster(props: { closeButton: boolean, richColors: boolean, position: string }) {
+    return null;
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <main>
-                <SidebarTrigger />
-                {children}
-            </main>
+            <SidebarInset className="flex flex-col h-screen overflow-hidden">
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <div className="w-full h-full flex items-center">
+                        <div className="flex-1">
+
+                        </div>
+                        <ModeToggle></ModeToggle>
+                    </div>
+                </header>
+                <div className="flex flex-1 flex-col overflow-y-auto">
+                    {children}
+                </div>
+            </SidebarInset>
         </SidebarProvider>
     )
 }
