611991be6c039f940209cbd4ebb7ef7b1d93d108
mehdighallam6 <mehdighallam6@gmail.com>
2025-03-06 00:38:41 +0000

    Remove terminal

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -40,55 +40,6 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="bg-white dark:bg-black footer"></div>
-                <div className="flex flex-col items-center mx-auto max-w-7xl py-5">
-                    <Terminal>
-                        <AnimatedSpan delay={1500} className="text-green-500">
-                            <span>✔ Preflight checks.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={2000} className="text-green-500">
-                            <span>✔ Verifying framework. Found Laravel.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={2500} className="text-green-500">
-                            <span>✔ Validating Tailwind CSS.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={3000} className="text-green-500">
-                            <span>✔ Validating import alias.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={3500} className="text-green-500">
-                            <span>✔ Writing components.json.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={4000} className="text-green-500">
-                            <span>✔ Checking registry.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={4500} className="text-green-500">
-                            <span>✔ Updating tailwind.config.ts</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={5000} className="text-green-500">
-                            <span>✔ Updating app/globals.css</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={5500} className="text-green-500">
-                            <span>✔ Installing dependencies.</span>
-                        </AnimatedSpan>
-
-                        <AnimatedSpan delay={6000} className="text-blue-500">
-                            <span>ℹ Updated 1 file:</span>
-                            <span className="pl-2">- lib/utils.ts</span>
-                        </AnimatedSpan>
-
-                        <TypingAnimation delay={6500} className="text-muted-foreground">
-                            Success! Project initialization completed.
-                        </TypingAnimation>
-                        
-                    </Terminal>
-                </div>
             </div>
         </>
     );
