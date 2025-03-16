5f0f9f4d5ba3fe448f3d37bcb9b4143c81712173
mehdighallam6 <mehdighallam6@gmail.com>
2025-03-16 22:17:01 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,12 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class,'home'])->name('home');
 
+Route::middleware(['auth','verified'])->group(function () {
+    Route::get('/dashboard', [HomeController::class,'dashboard'])->name('dashboard');
+    Route::resource('users', UserController::class);
+});
 
 Route::middleware('auth')->group(function () {
-
-    Route::middleware('verified')->group(function () {
-        Route::get('/dashboard', [HomeController::class,'dashboard'])->name('dashboard');
-        Route::resource('users', UserController::class);
-    });
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
